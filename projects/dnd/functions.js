3b55f855b93227e160a26d7dfece0c274e4ba0c9
ToyTox <toytox@gmail.com>
2021-10-20 22:12:19 +0300

    Fix 7 task

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -75,7 +75,11 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-  target.addEventListener('click', () => fn(), { once: true });
+  function clickOnce() {
+    fn();
+    target.removeEventListener('click', clickOnce);
+  }
+  target.addEventListener('click', clickOnce);
 }
 
 export { addListener, removeListener, skipDefault, emulateClick, delegate, once };
